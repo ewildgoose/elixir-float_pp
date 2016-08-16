0c6980f147d954cbd9e325c4084753c568267cae
Kip Cole <kipcole9@gmail.com>
2016-08-17 03:41:59 +0800

    Remove Elixir 1.4 warnings

--- mix.exs
+++ mix.exs
@@ -7,8 +7,8 @@ defmodule Float.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps,
-     package: package]
+     deps: deps(),
+     package: package()]
   end
 
   # Configuration for the OTP application
